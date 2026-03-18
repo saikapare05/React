6f9d9fa3bb5d3f054dd44057d6a81b771b4eb306
sai kapare <kaparesai620@gmail.com>
2026-03-18 10:38:15 +0530

    Folder-1 Done

---
+++ Project_1/folder-1/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div id="parent">
+      <h1 id="child-1">Hello, World!</h1>
+      <h1 id="child-2">Welcome to React!</h1>
+    </div>
+  )
+}
+
+export default App
